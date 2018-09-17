2a0e3a75b6bf2c3b41e3498ea0eedf91218a27d7
Abhinav Upadhyay <abhinav.upadhyay@agilitix.ai>
2018-09-17 17:55:14 +0530

    Bump version to 0.2.0

--- apptuit/__init__.py
+++ apptuit/__init__.py
@@ -1,5 +1,5 @@
 from .apptuit_client import Apptuit, DataPoint
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 __all__ = ['Apptuit', 'DataPoint']
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt', 'r') as f:
 setup(
     name="apptuit",
     packages=['apptuit'],
-    version="0.1.0",
+    version="0.2.0",
     description="Apptuit Python Client",
     author="Abhinav Upadhyay",
     author_email="abhinav.updadhyay@agiltix.ai",
