97d1495018d8e03cab34720b1781f49e5ac51019
Abhinav Upadhyay <abhinav.upadhyay@agilitix.ai>
2019-03-29 12:29:31 +0530

    Python 3.4 is no longer supported
    (add python 3.7 to travis build)

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
     license="Apache License 2.0",
     classifiers=[
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
