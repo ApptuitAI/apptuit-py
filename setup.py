2be50100a803e55c761836e3a6ebf01d225ec488
Abhinav Upadhyay <abhinav.upadhyay@agilitix.ai>
2019-07-23 13:39:02 +0530

    [skip travis] Bump to 2.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="apptuit",
     packages=['apptuit', 'apptuit.pyformance'],
-    version="2.0.0",
+    version="2.1.0",
     description="Apptuit Python Client",
     url="https://github.com/ApptuitAI/apptuit-py",
     author="Abhinav Upadhyay",
