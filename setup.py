ad2cd43bffec59b68eb0b1d1d7dee3bb356634bb
Abhinav Upadhyay <abhinav.upadhyay@agilitix.ai>
2019-10-18 12:29:37 +0530

    Bump to 2.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="apptuit",
     packages=['apptuit', 'apptuit.pyformance'],
-    version="2.3.0",
+    version="2.4.0",
     description="Apptuit Python Client",
     url="https://github.com/ApptuitAI/apptuit-py",
     author="Abhinav Upadhyay",
