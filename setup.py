9cdce4d6db983b73c7f739e8be7c292aae498c5f
Abhinav Upadhyay <abhinav.upadhyay@agilitix.ai>
2020-02-24 16:16:41 +0530

    Change version to 2.4.1 since its just requirements update
    Also, min required versions for other dependencies

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="apptuit",
     packages=['apptuit', 'apptuit.pyformance'],
-    version="2.5.0",
+    version="2.4.1",
     description="Apptuit Python Client",
     url="https://github.com/ApptuitAI/apptuit-py",
     author="Abhinav Upadhyay",
@@ -21,7 +21,7 @@ setup(
         ],
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    install_requires=['requests', 'pyformance', 'backports.functools_lru_cache;python_version<"3"'],
+    install_requires=['requests>=2.14.0', 'pyformance>=0.4', 'backports.functools_lru_cache;python_version<"3"'],
     tests_require=['mock;python_version<"3.3"', 'nose', 'pandas', 'numpy'],
     test_suite='nose.collector',
     data_files=['LICENSE']
